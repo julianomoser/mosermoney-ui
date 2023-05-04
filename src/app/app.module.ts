b884553b8e7fae926a657f07020a593def6459eb
Juliano Moser <juliano.moserjj@gmail.com>
2023-05-04 18:36:18 -0300

    Implementado o formulário de pesquisa de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { TabViewModule } from 'primeng/tabview';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TabViewModule
+    ButtonModule,
+    InputTextModule,
+    TabViewModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
