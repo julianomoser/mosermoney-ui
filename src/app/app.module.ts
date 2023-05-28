7a8122d124b7b0d1729a47fed7d14344cb1810f1
Juliano Moser <juliano.moserjj@gmail.com>
2023-05-28 04:46:00 -0300

    Implementado Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
+import { CoreModule } from './core/core.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { FormsModule } from '@angular/forms';
@@ -12,13 +12,13 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
 
+    CoreModule,
     LancamentosModule,
     PessoasModule,
   ],
