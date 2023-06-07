0456068a65af4b6266cf14b0cd770c5428087c9b
Juliano Moser <juliano.moserjj@gmail.com>
2023-06-07 18:20:27 -0300

    Revert "Alterando o locale da aplicação para pt-BR"
    This reverts commit 70b7de03c2cc81ae6952c5fcb5c904dd4b6d1479.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { PrimeNGConfig } from 'primeng/api';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'mosermoney-ui';
-
-  constructor(
-    private config: PrimeNGConfig,
-    private translateService: TranslateService
-  ) { }
-
-  ngOnInit() {
-    this.translateService.setDefaultLang('pt');
-    this.translateService.get('primeng')
-      .subscribe(res => this.config.setTranslation(res));
-  }
 }
