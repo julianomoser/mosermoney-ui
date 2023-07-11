218ab922ea12b8c6126fe58ec2978f6b302c1ce0
Juliano Moser <juliano.moserjj@gmail.com>
2023-07-11 16:52:37 -0300

    Implementado serviço de listagem de categorias

---
+++ src/app/categorias/categoria.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  categoriasUrl = environment.API_URL + '/categorias';
+  basicAuth = environment.BASIC_AUTH;
+
+  constructor(
+    private http: HttpClient
+    ) { }
+
+  listarTodas(): Promise<any> {
+    const headers = new HttpHeaders()
+      .append('Authorization', this.basicAuth);
+
+    return firstValueFrom(
+      this.http.get(this.categoriasUrl, { headers })
+    );
+  }
+}
